2ef28ca849f7a4850ebdd454aea98b73b1c46cad
Audrey V <avv236@nyu.edu>
2016-02-25 16:21:42 -0500

    First Upload

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// values are 0 if mouse isn't moving
+		float mouseX = Input.GetAxis("Mouse X"); // moving left = -1
+		float mouseY = Input.GetAxis("Mouse Y"); // moving down = -1
+
+		float rotationY;
+		// horizontal turning
+		transform.Rotate(mouseY * Time.deltaTime * 90f, mouseX * Time.deltaTime * 90f, 0f);
+
+		// horizontal movement
+		transform.Rotate (0f, mouseX * Time.deltaTime * 90f, 0f);
+
+		// veritcal movement
+		rotationY =+ -mouseY * Time.deltaTime* 90f;
+		rotationY = Mathf.Clamp(rotationY, -80f, 80f);
+			// so we don't look upside down
+		transform.localEulerAngles = new Vector3
+			(rotationY, transform.localEulerAngles.y, 0f); 
+			// setting Z to 0 unrolls camera
+		// hide cursor and lock on screen center
+		if (Input.GetMouseButtonDown(0)){
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+			
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	bool sittingUp = false;
+
+	public GameObject rLeg;
+	public GameObject lLeg;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// SITTING UP SECTION
+		if(Input.GetKeyDown(KeyCode.Space) && sittingUp == false ){
+			transform.Rotate(10f, 0f, 0f);
+		}
+
+		if(sittingUp == false && transform.localEulerAngles.x > 5f){
+			transform.Rotate(-45f * Time.deltaTime, 0f, 0f);
+		}
+			
+		if(transform.localEulerAngles.x >= 80f){
+			sittingUp = true;
+			transform.Rotate(-10f * Time.deltaTime, 0f, 0f);
+		}
+
+		//MOVING LEGS SECTION
+
+		if(Input.GetKeyDown(KeyCode.RightArrow) && sittingUp == true){
+			rLeg.transform.Rotate(0f, 10f, 0f);
+		}
+
+		if(Input.GetKeyDown(KeyCode.UpArrow) && sittingUp == true){
+			lLeg.transform.Rotate(0f, 10f, 0f);
+		}
+	}
+}
